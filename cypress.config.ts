69da29d8831e18bc1c13f3a6242e092109df8abc
Natalia <nataliakrzciuk@Mac.NAT.warszawa.vectranet.pl>
2025-07-30 14:11:46 +0200

    Final fixes

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,8 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://www.saucedemo.com',
+    screenshotsFolder: 'cypress/screenshots',
+    video: false ,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
 });
--- cypress/e2e/auth.cy.ts
+++ cypress/e2e/auth.cy.ts
@@ -6,11 +6,12 @@ describe('Authentication tests', () => {
   });
 
   it('TC01 – Login succeeds with valid credentials', () => {
-    loginPage.login('standard_user', 'secret_sauce');
+    cy.fixture('users').then(users => loginPage.login(users.standard.user, users.standard.pass));
     cy.url().should('include', '/inventory.html');
   });
 
   it('TC02 – Login fails with invalid username', () => {
+    cy.fixture('users').then(users => loginPage.login(users.invalid.user, users.standard.pass));
     loginPage.login('invalid_user', 'secret_sauce');
     loginPage.elements.errorMessage()
       .should('be.visible')
@@ -18,21 +19,21 @@ describe('Authentication tests', () => {
   });
 
   it('TC03 – Login fails with invalid password', () => {
-    loginPage.login('standard_user', 'wrong_password');
+    cy.fixture('users').then(users => loginPage.login(users.standard.user, users.invalid.pass));
     loginPage.elements.errorMessage()
       .should('be.visible')
       .and('contain.text', 'Epic sadface: Username and password do not match any user in this service');
   });
 
   it('TC04 – Login fails with missing username', () => {
-    loginPage.login(undefined, 'secret_sauce');
+    cy.fixture('users').then(users => loginPage.login(undefined, users.standard.pass));
     loginPage.elements.errorMessage()
       .should('be.visible')
       .and('contain.text', 'Epic sadface: Username is required');
   });
 
   it('TC05 – Login fails with missing password', () => {
-    loginPage.login('standard_user', undefined);
+    cy.fixture('users').then(users => loginPage.login(users.standard.user, undefined));
     loginPage.elements.errorMessage()
       .should('be.visible')
       .and('contain.text', 'Epic sadface: Password is required');
